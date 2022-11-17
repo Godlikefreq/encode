0ce84e9283cdeaaf068585c22a9f177cf6ef8dbc
Godlikefreq <godlikefreq@gmail.com>
2022-11-17 22:45:43 +0300

    Added SHA2

--- encode.rb
+++ encode.rb
@@ -5,15 +5,18 @@ user_input = STDIN.gets.chomp
 
 puts "Каким способом зашифровать:
 1. MD5
-2. SHA1"
+2. SHA1
+3. SHA2"
 
 user_choice = STDIN.gets.to_i
 
 result =
   if user_choice == 1
     Digest::MD5.hexdigest(user_input)
-  else
+  elsif user_choice == 2
     Digest::SHA1.hexdigest(user_input)
+  else
+    Digest::SHA2.hexdigest(user_input)
   end
 
 puts "Вот что получилось: #{result}"
