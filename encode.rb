f4fbaa3338cc6d12576751cb27abfcd1bc4b5c0f
Godlikefreq <godlikefreq@gmail.com>
2022-11-17 22:40:44 +0300

    Initial Commit

---
+++ encode.rb
@@ -0,0 +1,19 @@
+require 'Digest'
+
+puts "Введите слово или фразу для шифрования:"
+user_input = STDIN.gets.chomp
+
+puts "Каким способом зашифровать:
+1. MD5
+2. SHA1"
+
+user_choice = STDIN.gets.to_i
+
+result =
+  if user_choice == 1
+    Digest::MD5.hexdigest(user_input)
+  else
+    Digest::SHA1.hexdigest(user_input)
+  end
+
+puts "Вот что получилось: #{result}"
